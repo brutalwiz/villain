24102bcf5f577e17483c1ee6cfdebd3c11803c73
Jay Feldmann <janlukasfeldmann@gmail.com>
2018-01-08 16:21:11 +0100

    Delayed Dash
    Dash is delayed and can be used in air

--- gamemode/dash.lua
+++ gamemode/dash.lua
@@ -1,18 +1,32 @@
+local delay = 0
+local spd = 2000
+
+function delayed()
+	delay = CurTime() + 2
+	print("Delayed".. delay)
+end
+
 hook.Add("KeyPress","DASH_BACKWARDS",function(ply,key)
-			if ply:OnGround() and key == IN_SPEED and ply:KeyDown(IN_BACK) then
-				ply:SetVelocity((ply:GetForward() * -0.75) * 2000);
-				ply:ViewPunch(Angle(-2,0,0)) --Added "Punch" animation for smoother feel
-			elseif ply:OnGround() and key == IN_SPEED and ply:KeyDown(IN_FORWARD) then
-				ply:SetVelocity((ply:GetForward() * 0.75) * 2000);
-				ply:ViewPunch(Angle(2,0,0))
-			elseif ply:OnGround() and key == IN_SPEED and ply:KeyDown(IN_MOVELEFT) then
-				ply:SetVelocity((ply:GetRight() * -0.75) * 2000);
-				ply:ViewPunch(Angle(0,0,-2))
-			elseif ply:OnGround() and key == IN_SPEED and ply:KeyDown(IN_MOVERIGHT) then
-				ply:SetVelocity((ply:GetRight() * 0.75) * 2000);
-				ply:ViewPunch(Angle(0,0,2))	
-			end
-			if not ply:OnGround() then
-				return
-			end		
+	if CurTime() < delay then return end
+	spd = 2000
+	if not ply:OnGround() then
+		spd = spd / 4
+	end 
+	if key == IN_SPEED and ply:KeyDown(IN_BACK) then
+		ply:SetVelocity((ply:GetForward() * -0.75) * spd);
+		ply:ViewPunch(Angle(-2,0,0)) --Added "Punch" animation for smoother feel
+		delayed()
+	elseif key == IN_SPEED and ply:KeyDown(IN_FORWARD) then
+		ply:SetVelocity((ply:GetForward() * 0.75) * spd);
+		ply:ViewPunch(Angle(2,0,0))
+		delayed()
+	elseif key == IN_SPEED and ply:KeyDown(IN_MOVELEFT) then
+		ply:SetVelocity((ply:GetRight() * -0.75) * spd);
+		ply:ViewPunch(Angle(0,0,-2))
+		delayed()
+	elseif key == IN_SPEED and ply:KeyDown(IN_MOVERIGHT) then
+		ply:SetVelocity((ply:GetRight() * 0.75) * spd);
+		ply:ViewPunch(Angle(0,0,2))	
+		delayed()
+	end	
 end)
