2bcac6bfe4f5100fc86317da62e456e313814541
schffp <35227426+schffp@users.noreply.github.com>
2018-01-09 18:02:12 +0100

    add alarm soundfiles to resources

--- gamemode/resources.lua
+++ gamemode/resources.lua
@@ -2,3 +2,10 @@ resource.AddWorkshop( "304266718" ) -- Desolation Map
 resource.AddWorkshop( "316414565" ) -- Shrekzooka
 resource.AddWorkshop( "108720350" ) -- AWP
 resource.AddWorkshop( "166165013" ) -- Hitler Modell
+
+--[[ alarm sounds --]]
+resource.AddFile("sounds/ps.wav")
+resource.AddFile("sounds/v.wav")
+resource.AddFile("sounds/l.wav")
+resource.AddFile("sounds/m.wav")
+resource.AddFile("sounds/n.wav")
